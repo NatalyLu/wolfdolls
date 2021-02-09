838e659bdf973f93dffa3b4818a1d96602c91d88
Natalia <lukashina_natalia@mil.ru>
2021-02-10 01:29:08 +0300

    added slick-slider

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    speed: 1000,
+    easing: 'ease',
+    autoplay: true,
+    autoplaySpeed: 1500,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+  });
+});
